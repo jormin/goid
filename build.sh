924f057e45a0c76b3425d0f5085231d4f8887fdf
Jormin <jorminxee@gmail.com>
2021-01-19 16:15:25 +0800

    :sparkles: feat(1.0.0): init
    Signed-off-by: Jormin <jorminxee@gmail.com>

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ cd cmd/tcp && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build id.go
 cd ../../ && docker build -t ccr.ccs.tencentyun.com/jormin/goid:latest .
 # 上传镜像
 docker push ccr.ccs.tencentyun.com/jormin/goid:latest
+# 删除生成的可执行文件
+rm cmd/tcp/id
