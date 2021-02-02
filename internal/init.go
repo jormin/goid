b53a8e14111279c9dfb82c749a0d459412323113
Jormin <jorminxee@gmail.com>
2021-02-02 14:45:56 +0800

    :sparkles: feat(1.0.0): log
    Signed-off-by: Jormin <jorminxee@gmail.com>

--- internal/init.go
+++ internal/init.go
@@ -2,9 +2,12 @@ package internal
 
 import (
 	"gitlab.wcxst.com/jormin/goid/internal/config"
+	"gitlab.wcxst.com/jormin/golog/log"
 )
 
 func Init() {
 	// 初始化配置
 	config.Init()
+	// 初始化日志
+	log.Init(&config.LogCfg)
 }
