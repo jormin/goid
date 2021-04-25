c8acab0ecd46436a22b64de84ec6098cbc6e4372
Jormin <jorminxee@gmail.com>
2021-04-25 18:47:26 +0800

    :sparkles: feat(1.0.0): change
    Signed-off-by: Jormin <jorminxee@gmail.com>

--- pkg/id/client_test.go
+++ pkg/id/client_test.go
@@ -9,7 +9,12 @@ import (
 // 测试生成ID
 func TestClientNewID(t *testing.T) {
 	// c, err := NewClient("127.0.0.1:8888")
-	c, err := NewClient("82.157.101.82:31339")
+	c, err := NewClient(
+		&Config{
+			Host: "82.157.101.82",
+			Port: 31339,
+		},
+	)
 	if err != nil {
 		t.Errorf("get id client error: %v", err)
 	}
