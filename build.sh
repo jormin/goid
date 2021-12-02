8d71c6c1e140c7e5ed55dd45f740e4fd26aba832
Jormin <jorminxee@gmail.com>
2021-12-02 21:41:33 +0800

    :sparkles: feat(1.0.0): cicd
    Signed-off-by: Jormin <jorminxee@gmail.com>

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ export PATH=$PATH:/usr/local/go/bin
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.io,direct
 go mod download
-go test -v ./... --cover
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o goid main.go
